8009187b95b98de129267221df630bce6c8114da
Jiahao Liu <jiahaoliuliu@gmail.com>
2014-03-11 19:12:56 +0100

    First commit

---
+++ src/com/jiahaoliuliu/viewpager/ViewPagerActivity.java
@@ -0,0 +1,15 @@
+package com.jiahaoliuliu.viewpager;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class ViewPagerActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.view_pager_layout);
+	}
+
+}
